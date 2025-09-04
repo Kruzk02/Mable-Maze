f63f1f28922378b7cc0ed07e0745b9ef69ff40f3
Thien phuc <thienphuc484@gmail.com>
2025-09-04 14:37:07 +0700

    Add Cell class

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,18 @@
+﻿
+public class Cell
+{
+    public int X { get; }
+    public int Z { get; }
+    
+    public bool Visited { get; set; }
+    public bool Top { get; set; } = true;
+    public bool Bottom { get; set; } = true;
+    public bool Left { get; set; } = true;
+    public bool Right { get; set; } = true;
+
+    public Cell(int x , int z)
+    {
+        X = x;
+        Z = z;
+    }
+}
