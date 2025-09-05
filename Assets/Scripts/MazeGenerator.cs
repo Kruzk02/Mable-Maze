ae77be4647c7e741102268211dd37fee56a69e29
Thien phuc <thienphuc484@gmail.com>
2025-09-05 12:16:49 +0700

    Add MazeGenerator for random generate maze path

--- Assets/Scripts/MazeGenerator.cs
+++ Assets/Scripts/MazeGenerator.cs
@@ -1,32 +1,40 @@
 ﻿
 using System;
 using System.Collections.Generic;
-using NUnit.Framework;
 
 public class MazeGenerator
 {
     private readonly int _rows, _cols;
-    private int[,] _grid;
-    private readonly bool[,] _visited;
     private readonly int _goalX, _goalZ;
     private readonly Random _random = new();
     
     private readonly int[] _directionX = { -1, 0, 1, 0 };
     private readonly int[] _directionZ = { 0, 1, 0, -1 };
+
+    private readonly Cell[,] _cell;
     
     public MazeGenerator(int rows, int cols)
     {
         _rows = rows;
         _cols = cols;
         
-        _visited = new  bool[_rows, _cols];
-        _grid = new int[_rows, _cols];
+        _cell = new Cell[rows, cols];
+
+        for (var x = 0; x < rows; x++)
+        {
+            for (var z = 0; z < cols; z++)
+            {
+                _cell[x, z] = new Cell(x, z);
+            }
+        }
+        
         Generate(_random.Next(_rows), _random.Next(_cols));
     }
 
     private void Generate(int x, int z)
     {
-        _visited[x, z] = true;
+        var current = _cell[x, z];
+        current.Visited = true;
 
         var dirs = new List<int>{0, 1, 2, 3};
         Shuffle(dirs);
@@ -35,17 +43,26 @@ public class MazeGenerator
         {
             var newX = x + _directionX[dir];
             var newZ = z + _directionZ[dir];
-
-            if (!IsValid(newX, newZ)) continue;
             
-            _visited[newX, newZ] = true;
-            Generate(newX, newZ);
+            if (IsValid(newX, newZ))
+            {
+                RemoveWall(current, _cell[newX, newZ]);
+                Generate(newX, newZ);
+            }
         }
     }
 
     private bool IsValid(int x, int z)
     {
-        return x >= 0 && z >= 0 && x < _rows && z < _cols && !_visited[x,z];
+        return x >= 0 && z >= 0 && x < _rows && z < _cols && !_cell[x,z].Visited;
+    }
+
+    private static void RemoveWall(Cell a, Cell b)
+    {
+        if (b.X == a.X - 1) { a.Top = false; b.Bottom = false; }
+        if (b.X == a.X + 1) { a.Bottom = false; b.Top = false; }
+        if (b.Z == a.Z - 1) { a.Left = false; b.Right = false; }
+        if (b.Z == a.Z + 1) { a.Right = false; b.Left = false; }
     }
 
     private void Shuffle(List<int> list)
@@ -56,4 +73,6 @@ public class MazeGenerator
             (list[i], list[j]) = (list[j], list[i]);
         }
     }
+
+    public Cell[,] GetCells() => _cell;
 }
