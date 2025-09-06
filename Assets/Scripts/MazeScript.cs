5b51506d383a911038504eb7cc8fe6d9d68872a2
Thien phuc <thienphuc484@gmail.com>
2025-09-06 12:55:10 +0700

    refactor MazeScript to made more readable

--- Assets/Scripts/MazeScript.cs
+++ Assets/Scripts/MazeScript.cs
@@ -1,39 +1,42 @@
 
-using System;
 using UnityEngine;
-using UnityEngine.InputSystem;
 using UnityEngine.Serialization;
 
 public class MazeScript : MonoBehaviour
 {
 
-    public GameObject wallPrefab;
+    [FormerlySerializedAs("_wallPrefab"), SerializeReference] private GameObject wallPrefab;
+
+    private const float WallOffset = 5.0f;
+    private const float CellSpacing = 0.55f;
+    private const float HalfCell = 0.5f;
+    private const int MazeBoundaryLimit = 5;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        var mazeGenerator = new MazeGenerator(7, 7);
+        var mazeGenerator = new MazeGenerator(10, 10);
         var maze = mazeGenerator.GetCells();
         DrawMaze(maze);
     }
 
     private void DrawMaze(Cell[,] cells)
     {
-        var rows = cells.GetLength(0);
-        var cols = cells.GetLength(1);
+        var cellRows = cells.GetLength(0);
+        var cellCols = cells.GetLength(1);
 
-        for (var x = 0; x < rows; x++)
+        for (var x = 0; x < cellRows; x++)
         {
-            for (var z = 0; z < cols; z++)
+            for (var z = 0; z < cellCols; z++)
             {
                 var cell = cells[x, z];
              
-                var pos = new Vector3(x * 0.55f, 0, z * 0.55f);
+                var pos = new Vector3(x * CellSpacing, 0, z * CellSpacing);
                 
-                if (cell.Top) CreateWall( pos + new Vector3(x - 5.0f, 0.5f, z - 0.5f - 5.0f), Quaternion.identity);
-                if (cell.Bottom) CreateWall(pos + new Vector3(x - 5.0f, 0.5f, z + 0.5f - 5.0f), Quaternion.identity);
-                if (cell.Left) CreateWall(pos + new Vector3(x - 0.5f - 5.0f, 0.5f, z - 5.0f), Quaternion.Euler(0, 90, 0));
-                if (cell.Right) CreateWall(pos + new Vector3(x + 0.5f - 5.0f, 0.5f, z - 5.0f), Quaternion.Euler(0, 90, 0));
+                if (cell.Top) CreateWall(pos + new Vector3(x - WallOffset, HalfCell, z - HalfCell - WallOffset), Quaternion.identity);
+                if (cell.Bottom) CreateWall(pos + new Vector3(x - WallOffset, HalfCell, z + HalfCell - WallOffset), Quaternion.identity);
+                if (cell.Left) CreateWall(pos + new Vector3(x - HalfCell - WallOffset, HalfCell, z - WallOffset), Quaternion.Euler(0, 90, 0));
+                if (cell.Right) CreateWall(pos + new Vector3(x + HalfCell - WallOffset, HalfCell, z - WallOffset), Quaternion.Euler(0, 90, 0));
             }
         }
     }
@@ -41,7 +44,9 @@ public class MazeScript : MonoBehaviour
     void CreateWall(Vector3 position, Quaternion rotation)
     {
         var wall = Instantiate(wallPrefab, position, rotation, transform);
-        if (wall.transform.position.x < -5 ||  wall.transform.position.x > 5 || wall.transform.position.z < -5 || wall.transform.position.z > 5) Destroy(wall);   
+        if (
+            wall.transform.position.x <= -MazeBoundaryLimit || wall.transform.position.x >= MazeBoundaryLimit || 
+            wall.transform.position.z <= -MazeBoundaryLimit || wall.transform.position.z >= MazeBoundaryLimit) Destroy(wall);   
     }
 
 
