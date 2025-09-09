551f4bc830c376722f5dd58f80fbfd1f453a92ae
Thien phuc <thienphuc484@gmail.com>
2025-09-09 14:12:57 +0700

    Add Respawn() in BallSpawner script

--- Assets/Scripts/BallSpawner.cs
+++ Assets/Scripts/BallSpawner.cs
@@ -4,8 +4,16 @@ using UnityEngine.Serialization;
 public class BallSpawner : MonoBehaviour
 {
     [FormerlySerializedAs("_ball"), SerializeField] private GameObject ballPrefab;
+
+    private GameObject Ball { get; set; }
     private void Start()
     {
-        Instantiate(ballPrefab, new Vector3(0, 2.0f, 0), Quaternion.identity);
+        Ball = Instantiate(ballPrefab, new Vector3(0, 2.0f, 0), Quaternion.identity);
+    }
+
+    public void Respawn()
+    {
+        Destroy(Ball);
+        Ball = Instantiate(ballPrefab, new Vector3(0, 2.0f, 0), Quaternion.identity);
     }
 }
