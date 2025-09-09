35f66fbdff3fd083cffe2db0cb3e0a2830d25caa
Thien phuc <thienphuc484@gmail.com>
2025-09-09 14:17:52 +0700

    Add Respawn() in HoleSpawner

--- Assets/Scripts/HoleSpawner.cs
+++ Assets/Scripts/HoleSpawner.cs
@@ -4,14 +4,21 @@ using Random = System.Random;
 
 public class HoleSpawner : MonoBehaviour
 {
-    [FormerlySerializedAs("_gloryHole"), SerializeField] private GameObject hole;
+    [FormerlySerializedAs("_gloryHole"), SerializeField] private GameObject holePrefab;
     
     private const float CellSpacing = 0.55f;
     private const float WallOffset = 5.0f;
     private readonly Random _random = new();
 
+    private GameObject Hole { get; set; }
     public void Initialize(Cell[,] cells)
     {
+        CreateHole(cells);
+    }
+
+    public void Respawn(Cell[,] cells)
+    {
+        Destroy(Hole);
         CreateHole(cells);
     }
 
@@ -29,6 +36,6 @@ public class HoleSpawner : MonoBehaviour
         } while (cells[x, z].Top && cells[x, z].Bottom && cells[x, z].Left && cells[x, z].Right);
 
         var pos = new Vector3(x * CellSpacing, 0.1f, z * CellSpacing - WallOffset);
-        Instantiate(hole, pos, Quaternion.identity, transform);
+        Hole = Instantiate(holePrefab, pos, Quaternion.identity, transform);
     }
 }
