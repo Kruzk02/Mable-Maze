f76f30ef746a528a050a7906127c67f323b5a016
Thien phuc <thienphuc484@gmail.com>
2025-09-16 13:23:48 +0700

    Add count down time for game over

--- Assets/Scripts/MazeManager.cs
+++ Assets/Scripts/MazeManager.cs
@@ -1,6 +1,8 @@
+using System.Net;
 using System.Threading.Tasks;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MazeManager : MonoBehaviour
 {
@@ -9,15 +11,39 @@ public class MazeManager : MonoBehaviour
     [SerializeField] private WallSpawner wallSpawner;
     [SerializeField] private BallSpawner ballSpawner;
     [SerializeField] private TextMeshProUGUI scoreUI;
-
+    [SerializeField] private TextMeshProUGUI timeUI;
+    [SerializeField] private float startTime = 30f;
+    
     private int _counter;
+    private float _timeRemaining;
     private void Start()
     {
+        _timeRemaining = startTime;
+        
         var mazeGenerator = new MazeGenerator(7,7);
         var cells = mazeGenerator.GetCells();
         
         holeSpawner.Initialize(cells);
         wallSpawner.Initialize(cells);
+    }
+
+    private void Update()
+    {
+        if (_timeRemaining > 0)
+        {
+            _timeRemaining -= Time.deltaTime;
+            var seconds = Mathf.CeilToInt(_timeRemaining);
+            timeUI.text = "";
+            timeUI.text = seconds.ToString();
+        }
+        else
+        {
+            timeUI.text = "";
+            timeUI.text = "0";
+            Time.timeScale = 0;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            Time.timeScale = 1;
+        }
     }
 
     private void OnEnable()
@@ -42,6 +68,7 @@ public class MazeManager : MonoBehaviour
         ballSpawner.Respawn();
         
         scoreUI.text = "";
+        _timeRemaining += 30;
         _counter++;
         scoreUI.text += _counter.ToString();
     }
