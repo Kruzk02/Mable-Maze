30644b65f0c3a405e21c935d7340892256f8b05c
Thien phuc <thienphuc484@gmail.com>
2026-04-08 12:00:28 +0700

    Add ScoreManager class

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ScoreManager
+{
+  private const string BestScoreKey = "BestScore";
+
+  public static int GetBestScore()
+  {
+    return PlayerPrefs.GetInt(BestScoreKey);
+  }
+
+  public static void SetBestScore(int score)
+  {
+    var bestScore = GetBestScore();
+    if (score > bestScore)
+    {
+      PlayerPrefs.SetInt(BestScoreKey, score);
+    }
+  }
+}
